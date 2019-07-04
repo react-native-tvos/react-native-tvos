fef188ba5ff10067d12e5f7d1b625bd99243754b
Douglas Lowder <dlowder@salesforce.com>
2019-07-03 18:02:16 -0700

    Fix tvOS tests

--- IntegrationTests/AccessibilityManagerTest.js
+++ IntegrationTests/AccessibilityManagerTest.js
@@ -11,7 +11,7 @@
 'use strict';
 
 const React = require('react');
-const ReactNative = require('react-native');
+const ReactNative = require('react-native-tvos');
 const {View} = ReactNative;
 const RCTDeviceEventEmitter = require('react-native/Libraries/EventEmitter/RCTDeviceEventEmitter');
 const {TestModule, AccessibilityManager} = ReactNative.NativeModules;
