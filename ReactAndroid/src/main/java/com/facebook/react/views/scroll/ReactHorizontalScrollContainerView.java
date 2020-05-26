c20d2f65cffc185fb7bbe5ec38786e93118cb834
Vince Dimitrov <4097884+vdmtrv@users.noreply.github.com>
2020-05-26 18:00:41 +0100

    Fix/android tv overflow and rtl (#66)
    * fix: horizontal list overflow beyond view bounds
    
    Signed-off-by: Ventsislav Dimitrov <4097884+vdmtrv@users.noreply.github.com>
    
    * fix: Android TV RTL support
    
    Signed-off-by: Ventsislav Dimitrov <4097884+vdmtrv@users.noreply.github.com>

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerView.java
@@ -17,7 +17,12 @@ import com.facebook.react.views.view.ReactViewGroup;
 public class ReactHorizontalScrollContainerView extends ReactViewGroup {
 
   private int mLayoutDirection;
-  private int mCurrentWidth;
+  private int mLastWidth = 0;
+  private Listener rtlListener = null;
+
+  public interface Listener {
+    void onLayout();
+  }
 
   public ReactHorizontalScrollContainerView(Context context) {
     super(context);
@@ -25,11 +30,15 @@ public class ReactHorizontalScrollContainerView extends ReactViewGroup {
         I18nUtil.getInstance().isRTL(context)
             ? ViewCompat.LAYOUT_DIRECTION_RTL
             : ViewCompat.LAYOUT_DIRECTION_LTR;
-    mCurrentWidth = 0;
   }
 
   @Override
   protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+    // This is to fix the overflowing (scaled) item being cropped
+    final HorizontalScrollView parent = (HorizontalScrollView) getParent();
+    parent.setClipChildren(false);
+    this.setClipChildren(false);
+
     if (mLayoutDirection == LAYOUT_DIRECTION_RTL) {
       // When the layout direction is RTL, we expect Yoga to give us a layout
       // that extends off the screen to the left so we re-center it with left=0
@@ -39,12 +48,25 @@ public class ReactHorizontalScrollContainerView extends ReactViewGroup {
       setLeft(newLeft);
       setRight(newRight);
 
+      // Fix the ScrollX position when using RTL language accounting for the case when new
+      // data is appended to the "end" (left) of the view (e.g. after fetching additional items)
+      final int offsetX = this.getMeasuredWidth() - mLastWidth + parent.getScrollX();
+
       // Call with the present values in order to re-layout if necessary
-      HorizontalScrollView parent = (HorizontalScrollView) getParent();
-      // Fix the ScrollX position when using RTL language
-      int offsetX = parent.getScrollX() + getWidth() - mCurrentWidth;
       parent.scrollTo(offsetX, parent.getScrollY());
+      mLastWidth = this.getMeasuredWidth();
+
+      // Use the listener to adjust the scrollposition if new data was appended
+      if (rtlListener != null) {
+        rtlListener.onLayout();
+      }
     }
-    mCurrentWidth = getWidth();
+  }
+  public int getLastWidth() {
+    return mLastWidth;
+  }
+
+  public void setListener(Listener rtlListener) {
+    this.rtlListener = rtlListener;
   }
 }
