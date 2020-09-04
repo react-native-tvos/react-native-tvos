a0855f527215576b02496c98963c36d06abb836d
Douglas Lowder <dlowder@salesforce.com>
2020-09-03 18:31:54 -0700

    useTVEventHandler() (#86)
    - useTVEventHandler wrapper for the TVEventHandler class
    - example code
    - README update

---
+++ RNTester/js/examples/TVEventHandler/TVEventHandlerExample.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+
+'use strict';
+
+const React = require('react');
+const ReactNative = require('react-native');
+
+const {Platform, View, Text, TouchableOpacity, useTVEventHandler} = ReactNative;
+
+const TVEventHandlerView: () => React.Node = () => {
+  const [lastEventType, setLastEventType] = React.useState('');
+
+  const myTVEventHandler = evt => {
+    setLastEventType(evt.eventType);
+  };
+
+  if (Platform.isTV) {
+    useTVEventHandler(myTVEventHandler); // eslint-disable-line react-hooks/rules-of-hooks
+    return (
+      <View>
+        <TouchableOpacity onPress={() => {}}>
+          <Text>
+            This example enables an instance of TVEventHandler to show the last
+            event detected from the Apple TV Siri remote or from a keyboard.
+          </Text>
+        </TouchableOpacity>
+        <Text style={{color: 'blue'}}>{lastEventType}</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <Text>This example is intended to be run on Apple TV.</Text>
+      </View>
+    );
+  }
+};
+
+exports.framework = 'React';
+exports.title = 'TVEventHandler example';
+exports.description = 'iOS alerts and action sheets';
+exports.examples = [
+  {
+    title: 'TVEventHandler',
+    render(): React.Node {
+      return <TVEventHandlerView />;
+    },
+  },
+];
