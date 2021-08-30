2570bc4244aa4af9b8811178c57bb12a4d3c78b4
Ganesh-Kumar-Reddy-M <89786399+Ganesh-Kumar-Reddy-M@users.noreply.github.com>
2021-08-31 02:11:58 +0530

    Fix the issue#216: tvOS - UI Layout is incorrect for TVFocusGuideView. (#217)
    For example, if the client specifies 'marginLeft' property in the style prop, then the TVFocusGuideView will apply the 'marginLeft' for both parent and child.
    And so, the left margin is getting added twice and the UI becomes incorrect. The same is applicable for other layout properties. So, must override the Layout for the 'RNFocusGuide' to avoid UI issues.
    
    Co-authored-by: Ganesh Kumar Reddy <ganeshkumar.reddymur@dish.com>

--- Libraries/Components/AppleTV/TVFocusGuideView.js
+++ Libraries/Components/AppleTV/TVFocusGuideView.js
@@ -42,9 +42,19 @@ class TVFocusGuideView extends React.Component<FocusGuideProps> {
     return (
       // Container view must have nonzero size
       <ReactNative.View style={[{minHeight: 1, minWidth: 1}, this.props.style]}>
+        {
+          /**
+           * The client specified layout(using 'style' prop) should be applied the container view ReactNative.View.
+           * And the focusGuide's layout shoule be overrided to wrap it fully inside the container view.
+           * For example, if the client specifies 'marginLeft' property in the style prop, 
+           * then the TVFocusGuideView will apply the 'marginLeft' for both the parentView and the focusGuideView.
+           * and so, the left margin is getting added twice and UI becomes incorrect. 
+           * The same is applicable for other layout properties.
+           */
+        }
         {Platform.isTVOS ? (
           <RNFocusGuide
-            style={this.props.style}
+            style={[this.props.style, styles.focusGuideLayout]}
             ref={ref => (this._focusGuideRef = ref)}
             destinationTags={this._destinationTags}>
             {this.props.children}
@@ -55,6 +65,19 @@ class TVFocusGuideView extends React.Component<FocusGuideProps> {
       </ReactNative.View>)
   }
 }
+
+const styles = ReactNative.StyleSheet.create({
+  focusGuideLayout: {
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    marginLeft: 0,
+    marginTop: 0,
+    marginRight: 0,
+    marginBottom: 0,
+  },
+});
 
 const RNFocusGuide = requireNativeComponent('RCTTVFocusGuideView');
 
