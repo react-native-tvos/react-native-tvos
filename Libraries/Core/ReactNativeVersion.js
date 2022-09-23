b9ea0fe509e8ad632bd9bd1487712df3e0744d4f
Douglas Lowder <douglowder@mac.com>
2022-09-23 13:30:36 -0700

    Bump version number (0.69.5-1)

--- Libraries/Core/ReactNativeVersion.js
+++ Libraries/Core/ReactNativeVersion.js
@@ -13,5 +13,5 @@ exports.version = {
   major: 0,
   minor: 69,
   patch: 5,
-  prerelease: null,
+  prerelease: '1',
 };
--- ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/ReactNativeVersion.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/ReactNativeVersion.java
@@ -18,5 +18,5 @@ public class ReactNativeVersion {
       "major", 0,
       "minor", 69,
       "patch", 5,
-      "prerelease", null);
+      "prerelease", "1");
 }
