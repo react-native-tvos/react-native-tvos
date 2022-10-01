71db428a50bcb7bc60e3b0db08109ade6d418fb9
Douglas Lowder <douglowder@mac.com>
2022-10-01 16:17:26 -0700

    Add hermesc to TV scripts; clean up scripts

--- scripts/prepare-tv-release.js
+++ scripts/prepare-tv-release.js
@@ -10,70 +10,47 @@
 'use strict';
 
 /**
- * This script prepares a release version of react-native and may publish to NPM.
- * It is supposed to run in CI environment, not on a developer's machine.
+ * This script is derived from npm-publish.js.
+ * It prepares a release version of react-native-tvos.
  *
- * For a dry run (commitly), this script will:
- *  * Version the commitly of the form `1000.0.0-<commitSha>`
  *  * Create Android artifacts
+ *  * Copy hermesc from react-native core release
  *  * It will not publish to npm
  *
- * For a nightly run, this script will:
- *  * Version the nightly release of the form `0.0.0-<dateIdentifier>-<commitSha>`
- *  * Create Android artifacts
- *  * Publish to npm using `nightly` tag
- *
- * For a release run, this script will:
- *  * Version the release by the tag version that triggered CI
- *  * Create Android artifacts
- *  * Publish to npm
- *     * using `latest` tag if commit is currently tagged `latest`
- *     * or otherwise `{major}.{minor}-stable`
  */
 
 const {exec, echo, exit, test, env} = require('shelljs');
-const {parseVersion, isTaggedLatest} = require('./version-utils');
 const fs = require('fs');
+const fs_extra = require('fs-extra');
 const os = require('os');
 const path = require('path');
-const yargs = require('yargs');
 
-const buildTag = process.env.CIRCLE_TAG;
-const otp = process.env.NPM_CONFIG_OTP;
-
-const includeHermes = true;
-const isCommitly = false;
-const dryRunBuild = false;
-
-if (includeHermes) {
+{
   const HERMES_INSTALL_LOCATION = 'sdks';
   const HERMES_SOURCE_DEST_PATH = `${HERMES_INSTALL_LOCATION}/hermes`;
 
   let hermesReleaseTag;
   let hermesReleaseURI;
-  if (isCommitly) {
-    // use latest commit / tarball
-    hermesReleaseURI = 'https://github.com/facebook/hermes/tarball/main';
-  } else {
-    // use one configured in disk
-    fs.readFile(
-      `${HERMES_INSTALL_LOCATION}/.hermesversion`,
-      {
+  try {
+    hermesReleaseTag = fs
+      .readFileSync(`${HERMES_INSTALL_LOCATION}/.hermesversion`, {
         encoding: 'utf8',
         flag: 'r',
-      },
-      function (err, data) {
-        if (err) {
-          echo('Failed to read current Hermes release tag.');
-          // TODO: We'll need to make sure every release going forward has one of these.
-          exit(1);
-        } else {
-          hermesReleaseTag = data.trim();
-          hermesReleaseURI = `https://github.com/facebook/hermes/archive/refs/tags/${hermesReleaseTag}.tar.gz`;
-        }
-      },
-    );
+      })
+      .trim();
+    hermesReleaseURI = `https://github.com/facebook/hermes/archive/refs/tags/${hermesReleaseTag}.tar.gz`;
+  } catch (err) {
+    echo('Failed to read current Hermes release tag.');
+    // TODO: We'll need to make sure every release going forward has one of these.
+    exit(1);
   }
+
+  // Copy hermesc from RN core release
+  const HERMESC_SOURCE_LOCATION = 'node_modules/react-native-core/sdks/hermesc';
+  const HERMESC_DEST_LOCATION = `${HERMES_INSTALL_LOCATION}/hermesc`;
+  fs_extra.copySync(HERMESC_SOURCE_LOCATION, HERMESC_DEST_LOCATION, {
+    overwrite: true,
+  });
 
   const tmpDownloadDir = fs.mkdtempSync(
     path.join(os.tmpdir(), 'hermes-tarball'),
@@ -100,58 +77,7 @@ if (includeHermes) {
   }
 }
 
-// 34c034298dc9cad5a4553964a5a324450fda0385
-const currentCommit = exec('git rev-parse HEAD', {
-  silent: true,
-}).stdout.trim();
-const shortCommit = currentCommit.slice(0, 9);
-
-const rawVersion =
-  // 0.0.0 triggers issues with cocoapods for codegen when building template project.
-  dryRunBuild
-    ? '1000.0.0'
-    : // For nightly we continue to use 0.0.0 for clarity for npm
-    nightlyBuild
-    ? '0.0.0'
-    : // For pre-release and stable releases, we use the git tag of the version we're releasing (set in bump-oss-version)
-      buildTag;
-
-let version,
-  major,
-  minor,
-  prerelease = null;
-try {
-  ({version, major, minor, prerelease} = parseVersion(rawVersion));
-} catch (e) {
-  echo(e.message);
-  exit(1);
-}
-
-let releaseVersion;
-if (dryRunBuild) {
-  releaseVersion = `${version}-${shortCommit}`;
-} else if (nightlyBuild) {
-  // 2021-09-28T05:38:40.669Z -> 20210928-0538
-  const dateIdentifier = new Date()
-    .toISOString()
-    .slice(0, -8)
-    .replace(/[-:]/g, '')
-    .replace(/[T]/g, '-');
-  releaseVersion = `${version}-${dateIdentifier}-${shortCommit}`;
-} else {
-  releaseVersion = version;
-}
-
-// Bump version number in various files (package.json, gradle.properties etc)
-// For stable, pre-release releases, we rely on CircleCI job `prepare_package_for_release` to handle this
-if (isCommitly) {
-  if (
-    exec(`node scripts/set-rn-version.js --to-version ${releaseVersion}`).code
-  ) {
-    echo(`Failed to set version number to ${releaseVersion}`);
-    exit(1);
-  }
-}
+const releaseVersion = require('../package.json').version;
 
 // -------- Generating Android Artifacts with JavaDoc
 if (exec('./gradlew :ReactAndroid:installArchives').code) {
@@ -196,34 +122,5 @@ artifacts.forEach(name => {
   }
 });
 
-if (dryRunBuild) {
-  echo('Skipping `npm publish` because --dry-run is set.');
-  exit(0);
-}
-
-// Running to see if this commit has been git tagged as `latest`
-const isLatest = isTaggedLatest(currentCommit);
-
-const releaseBranch = `${major}.${minor}-stable`;
-
-// Set the right tag for nightly and prerelease builds
-// If a release is not git-tagged as `latest` we use `releaseBranch` to prevent
-// npm from overriding the current `latest` version tag, which it will do if no tag is set.
-const tagFlag = nightlyBuild
-  ? '--tag nightly'
-  : prerelease != null
-  ? '--tag next'
-  : isLatest
-  ? '--tag latest'
-  : `--tag ${releaseBranch}`;
-
-// use otp from envvars if available
-const otpFlag = otp ? `--otp ${otp}` : '';
-
-if (exec(`npm publish ${tagFlag} ${otpFlag}`).code) {
-  echo('Failed to publish package to npm');
-  exit(1);
-} else {
-  echo(`Published to npm ${releaseVersion}`);
-  exit(0);
-}
+echo(`Release prepared for version ${releaseVersion}`);
+exit(0);
