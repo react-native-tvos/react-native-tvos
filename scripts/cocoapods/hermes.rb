a0b0adfcf65140dc91d64e90005a44590f4c250e
Douglas Lowder <douglowder@mac.com>
2022-10-26 12:55:01 -0700

    Add libevent podspec modified for TV

--- scripts/cocoapods/hermes.rb
+++ scripts/cocoapods/hermes.rb
@@ -15,6 +15,8 @@ def install_hermes_if_enabled(hermes_enabled, react_native_path)
     abort unless prep_status == 0
 
     pod 'React-hermes', :path => "#{react_native_path}/ReactCommon/hermes"
-    pod 'libevent', '~> 2.1.12'
+    # For TV, we restore the old podspec that was removed in https://github.com/facebook/react-native/pull/31010,
+    # update it to version 2.1.12, and add :tvos to platforms
+    pod 'libevent', :path => "#{react_native_path}/third-party-podspecs/libevent.podspec"
     pod 'hermes-engine', :podspec => "#{react_native_path}/sdks/hermes/hermes-engine.podspec"
 end
