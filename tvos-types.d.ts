c9f40f7c80d55f52e56e61fce56e5333a1c848f6
Henning Hall <henning.hall@hotmail.com>
2022-12-26 22:49:57 +0100

    Update typescript types to support react-native RN 0.69 (#400)
    Adding back the typescript types `hasTVPreferredFocus` `tvParallaxProperties` that now has been removed from the react-native main repo.

--- tvos-types.d.ts
+++ tvos-types.d.ts
@@ -127,4 +127,30 @@ declare module 'react-native' {
   export interface PressableStateCallbackType {
     readonly focused: boolean;
   }
+
+  export interface TouchableWithoutFeedbackPropsIOS {
+    /**
+     * *(Apple TV only)* TV preferred focus (see documentation for the View component).
+     *
+     * @platform ios
+     */
+    hasTVPreferredFocus?: boolean;
+
+    /**
+     * *(Apple TV only)* Object with properties to control Apple TV parallax effects.
+     *
+     * enabled: If true, parallax effects are enabled.  Defaults to true.
+     * shiftDistanceX: Defaults to 2.0.
+     * shiftDistanceY: Defaults to 2.0.
+     * tiltAngle: Defaults to 0.05.
+     * magnification: Defaults to 1.0.
+     * pressMagnification: Defaults to 1.0.
+     * pressDuration: Defaults to 0.3.
+     * pressDelay: Defaults to 0.0.
+     *
+     * @platform ios
+     */
+    tvParallaxProperties?: TVParallaxProperties;
+  }
+
 }
