baa3475762ae550804114e4c6ada813224a5c11e
Douglas Lowder <douglowder@mac.com>
2023-04-26 14:02:53 -0700

    Fix missing import in Android template

--- template/android/app/src/main/java/com/helloworld/MainApplication.java
+++ template/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -5,6 +5,7 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
