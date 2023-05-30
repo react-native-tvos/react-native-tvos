2a3b988c19356f9ff546eb18ae0c324b4fdacb59
Douglas Lowder <douglowder@mac.com>
2023-05-30 15:22:26 -0700

    Bump version number (0.71.8-0rc4)

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -24,7 +24,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(71),
                   RCTVersionPatch: @(8),
-                  RCTVersionPrerelease: @"0rc3",
+                  RCTVersionPrerelease: @"0rc4",
                   };
   });
   return __rnVersion;
