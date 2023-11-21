42a6dcf5cb497a4dbe7fcc6712cddbe65aa07da4
Douglas Lowder <douglowder@mac.com>
2023-11-21 11:50:03 -0800

    Set core 0.73 dependency to rc.5

--- scripts/set-rn-version.js
+++ scripts/set-rn-version.js
@@ -146,7 +146,7 @@ function setPackage({version}, dependencyVersions, argVersion) {
   packageJson.version = version;
 
   // Temporary until RN core is released
-  const coreVersion = argVersion.split('-')[0] + '-rc.4';
+  const coreVersion = argVersion.split('-')[0] + '-rc.5';
   packageJson.devDependencies = packageJson.devDependencies ?? {};
   packageJson.devDependencies[
     'react-native-core'
