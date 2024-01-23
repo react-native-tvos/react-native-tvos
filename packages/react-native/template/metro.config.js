29cdd95b871f2d2753fb37254c8ea7ae0a692598
Douglas Lowder <douglowder@mac.com>
2024-01-22 16:35:12 -0800

    Apply suggestions from code review
        Co-authored-by: Alex Hunt <hello@alexhunt.io>

--- packages/react-native/template/metro.config.js
+++ packages/react-native/template/metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig} = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,27 +6,27 @@ const {getDefaultConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-// When enabled, the optional code below will allow Metro to resolve
-// and bundle source files with TV-specific extensions
-// (e.g., *.ios.tv.tsx, *.android.tv.tsx, *.tv.tsx)
-//
-// Metro will still resolve source files with standard extensions
-// as usual if TV-specific files are not found for a module.
-//
-// This code is not enabled by default, since it will impact bundling performance,
-// but is available for developers who need this capability.
-//
-/*
-if (process.env.BUILDING_FOR_TV) {
-  const originalSourceExts = config.resolver.sourceExts;
-  const tvSourceExts = [
-    ...originalSourceExts.map((e) => `tv.${e}`),
-    ...originalSourceExts,
-  ];
-  config.resolver.sourceExts = tvSourceExts;
-}
- */
+const config = {
+  // When enabled, the optional code below will allow Metro to resolve
+  // and bundle source files with TV-specific extensions
+  // (e.g., *.ios.tv.tsx, *.android.tv.tsx, *.tv.tsx)
+  //
+  // Metro will still resolve source files with standard extensions
+  // as usual if TV-specific files are not found for a module.
+  //
+  // This code is not enabled by default, since it will impact bundling performance,
+  // but is available for developers who need this capability.
+  //
+  // resolver: process.env.BUILDING_FOR_TV
+  //   ? {
+  //       sourceExts: [].concat(
+  //         defaultConfig.resolver.sourceExts.map(e => `tv.${e}`),
+  //         defaultConfig.resolver.sourceExts,
+  //       ),
+  //     }
+  //   : undefined,
+};
 
-module.exports = config;
+module.exports = mergeConfig(defaultConfig, config);
