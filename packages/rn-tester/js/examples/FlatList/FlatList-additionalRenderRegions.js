7568c092f473af99f9a2891febebeb866b16d837
Yusuf YILDIRIM <yusuf@tinythin.gs>
2024-02-26 15:04:18 +0100

    feat: improve the example

--- packages/rn-tester/js/examples/FlatList/FlatList-additionalRenderRegions.js
+++ packages/rn-tester/js/examples/FlatList/FlatList-additionalRenderRegions.js
@@ -13,7 +13,7 @@ import type {RNTesterModuleExample} from '../../types/RNTesterTypes';
 import {useRNTesterTheme} from '../../components/RNTesterTheme';
 import BaseFlatListExample from './BaseFlatListExample';
 import * as React from 'react';
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 import {
   FlatList,
   Pressable,
@@ -21,17 +21,25 @@ import {
   Text,
   Dimensions,
   TVFocusGuideView,
+  Switch,
 } from 'react-native';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 const scale = screenHeight / 1080;
 
+type ItemType = number;
+const data: ItemType[] = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24, 25,
+];
+
 const ITEM_SPACING = 5 * scale;
 const ITEM_WIDTH = 400 * scale;
 const ITEM_TOTAL_WIDTH = ITEM_WIDTH + ITEM_SPACING;
 const ITEM_HEIGHT = 100 * scale;
 const NUMBER_OF_VISIBLE_ITEMS = Math.ceil(screenWidth / ITEM_TOTAL_WIDTH);
+const MID_INDEX = Math.floor(data.length / 2);
 
 function Row({title, children}: {title: string, children: React.Node}) {
   return (
@@ -41,11 +49,6 @@ function Row({title, children}: {title: string, children: React.Node}) {
     </TVFocusGuideView>
   );
 }
-type ItemType = number;
-const data: ItemType[] = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23, 24, 25,
-];
 
 /**
  * Constructs a case that tests if `additionalRenderRegions` property works as expected.
@@ -60,6 +63,8 @@ const data: ItemType[] = [
 export function FlatList_additionalRenderRegions(): React.Node {
   const theme = useRNTesterTheme();
   const listRef = useRef<?FlatList<ItemType>>(null);
+  const [additionalRenderRegionsEnabled, setAdditionalRenderRegionsEnabled] =
+    useState(true);
 
   const renderItem: RenderItemType<ItemType> = ({index}) => (
     <Pressable
@@ -77,6 +82,14 @@ export function FlatList_additionalRenderRegions(): React.Node {
 
   return (
     <TVFocusGuideView autoFocus trapFocusLeft trapFocusRight>
+      <TVFocusGuideView style={styles.switchSection}>
+        <Text style={styles.switchText}>Enable `additionalRenderRegions`</Text>
+        <Switch
+          value={additionalRenderRegionsEnabled}
+          onValueChange={() => setAdditionalRenderRegionsEnabled(val => !val)}
+        />
+      </TVFocusGuideView>
+
       <Row title="Initial Row">
         <FlatList
           ref={listRef}
@@ -84,11 +97,13 @@ export function FlatList_additionalRenderRegions(): React.Node {
           data={data}
           renderItem={renderItem}
           windowSize={1}
-          initialScrollIndex={NUMBER_OF_VISIBLE_ITEMS}
-          contentOffset={{x: NUMBER_OF_VISIBLE_ITEMS * ITEM_TOTAL_WIDTH, y: 0}}
-          additionalRenderRegions={[
-            {first: 0, last: NUMBER_OF_VISIBLE_ITEMS - 1},
-          ]}
+          initialScrollIndex={MID_INDEX}
+          contentOffset={{x: MID_INDEX * ITEM_TOTAL_WIDTH, y: 0}}
+          additionalRenderRegions={
+            additionalRenderRegionsEnabled
+              ? [{first: 0, last: NUMBER_OF_VISIBLE_ITEMS - 1}]
+              : undefined
+          }
           getItemLayout={(_d, index) => ({
             length: ITEM_TOTAL_WIDTH,
             offset: index * ITEM_TOTAL_WIDTH,
@@ -118,4 +133,6 @@ const styles = StyleSheet.create({
   },
   pressableText: {fontSize: 24 * scale},
   rowTitle: {color: 'white', marginBottom: 5 * scale},
+  switchSection: {flexDirection: 'row', paddingTop: 10 * scale},
+  switchText: {color: 'white'},
 });
