6d96959be4be4b741c707da5d021addd257ca2b7
Douglas Lowder <douglowder@mac.com>
2024-04-24 02:23:31 -0700

    Revert "Remove usage of Android API <23 in ReactActivityDelegate (#39663)"
    This reverts commit 3664b8aa221bfc232b0f737ea97b2672a5509e35.

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactActivityDelegate.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactActivityDelegate.java
@@ -7,10 +7,12 @@
 
 package com.facebook.react;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import androidx.annotation.Nullable;
@@ -179,6 +181,7 @@ public class ReactActivityDelegate {
     mReactDelegate.onConfigurationChanged(newConfig);
   }
 
+  @TargetApi(Build.VERSION_CODES.M)
   public void requestPermissions(
       String[] permissions, int requestCode, PermissionListener listener) {
     mPermissionListener = listener;
