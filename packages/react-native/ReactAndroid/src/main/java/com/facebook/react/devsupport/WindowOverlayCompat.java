08209c849eb7df3cd6878c5a66a63fe5d913081c
Douglas Lowder <douglowder@mac.com>
2024-04-24 02:22:53 -0700

    Revert "Remove support for Android API < 23 in WindowOverlayCompat (#39673)"
    This reverts commit c359a44676db917d7fe62b8205ee76653fe529b0.

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/WindowOverlayCompat.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/WindowOverlayCompat.java
@@ -18,10 +18,15 @@ import com.facebook.infer.annotation.Nullsafe;
 /* package */ @Nullsafe(Nullsafe.Mode.LOCAL)
 class WindowOverlayCompat {
 
+  private static final int ANDROID_OREO = 26;
   private static final int TYPE_APPLICATION_OVERLAY = 2038;
 
+  static final int TYPE_SYSTEM_ALERT =
+      Build.VERSION.SDK_INT < ANDROID_OREO
+          ? WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+          : TYPE_APPLICATION_OVERLAY;
   static final int TYPE_SYSTEM_OVERLAY =
-      Build.VERSION.SDK_INT < Build.VERSION_CODES.O
+      Build.VERSION.SDK_INT < ANDROID_OREO
           ? WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY
           : TYPE_APPLICATION_OVERLAY;
 }
