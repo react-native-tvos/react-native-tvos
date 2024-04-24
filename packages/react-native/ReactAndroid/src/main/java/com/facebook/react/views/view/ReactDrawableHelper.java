2b660cf7624d97a7eeea13327d154cdda99df65d
Douglas Lowder <douglowder@mac.com>
2024-04-24 02:19:36 -0700

    Revert "Remove support for Android API < 23 in ReactDrawableHelper (#39667)"
    This reverts commit 120aab2e995d341e5f620475c7f2beaf61e47f5e.

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactDrawableHelper.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactDrawableHelper.java
@@ -13,6 +13,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
+import android.os.Build;
 import android.util.TypedValue;
 import androidx.annotation.Nullable;
 import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
@@ -75,7 +76,9 @@ public class ReactDrawableHelper {
   }
 
   private static Drawable setRadius(ReadableMap drawableDescriptionDict, Drawable drawable) {
-    if (drawableDescriptionDict.hasKey("rippleRadius") && drawable instanceof RippleDrawable) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+        && drawableDescriptionDict.hasKey("rippleRadius")
+        && drawable instanceof RippleDrawable) {
       RippleDrawable rippleDrawable = (RippleDrawable) drawable;
       double rippleRadius = drawableDescriptionDict.getDouble("rippleRadius");
       rippleDrawable.setRadius((int) PixelUtil.toPixelFromDIP(rippleRadius));
