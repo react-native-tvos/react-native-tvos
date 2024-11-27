06c8bcb4d8b32caab2da716480efe9b6dbcecb8f
Douglas Lowder <douglowder@mac.com>
2024-11-27 02:25:01 -0800

    TV changes: ReactAndroid plus Kotlin conversions

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ViewProps.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ViewProps.kt
@@ -211,6 +211,7 @@ public object ViewProps {
               FLEX_SHRINK,
               FLEX_WRAP,
               JUSTIFY_CONTENT,
+              OVERFLOW,
               ALIGN_CONTENT,
               DISPLAY, /* position */
               POSITION,
