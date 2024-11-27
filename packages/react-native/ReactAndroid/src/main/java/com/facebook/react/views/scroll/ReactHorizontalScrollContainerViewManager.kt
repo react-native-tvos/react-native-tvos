ba938fa6eecc3432a74a4fd2622626986cbb9b83
Douglas Lowder <douglowder@mac.com>
2024-11-27 02:25:01 -0800

    TV changes: ReactAndroid plus Kotlin conversions

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerViewManager.kt
@@ -8,41 +8,21 @@
 package com.facebook.react.views.scroll
 
 import com.facebook.react.module.annotations.ReactModule
-import com.facebook.react.uimanager.ReactStylesDiffMap
-import com.facebook.react.uimanager.StateWrapper
 import com.facebook.react.uimanager.ThemedReactContext
-import com.facebook.react.uimanager.common.UIManagerType
-import com.facebook.react.uimanager.common.ViewUtil
-import com.facebook.react.views.view.ReactViewGroup
-import com.facebook.react.views.view.ReactViewManager
+import com.facebook.react.views.view.ReactClippingViewManager
 
 /** View manager for {@link ReactHorizontalScrollContainerView} components. */
 @ReactModule(name = ReactHorizontalScrollContainerViewManager.REACT_CLASS)
-public class ReactHorizontalScrollContainerViewManager : ReactViewManager() {
+public class ReactHorizontalScrollContainerViewManager :
+    ReactClippingViewManager<ReactHorizontalScrollContainerLegacyView>() {
+
   override public fun getName(): String = REACT_CLASS
 
-  protected override fun createViewInstance(
-      reactTag: Int,
-      context: ThemedReactContext,
-      initialProps: ReactStylesDiffMap?,
-      stateWrapper: StateWrapper?
-  ): ReactViewGroup {
-    check(uiManagerType == null)
-    uiManagerType = ViewUtil.getUIManagerType(reactTag)
-    val view = super.createViewInstance(reactTag, context, initialProps, stateWrapper)
-    uiManagerType = null
-    return view
-  }
-
-  public override fun createViewInstance(context: ThemedReactContext): ReactViewGroup {
-    return when (checkNotNull(uiManagerType)) {
-      UIManagerType.FABRIC -> ReactViewGroup(context)
-      else -> ReactHorizontalScrollContainerLegacyView(context)
-    }
-  }
+  override public fun createViewInstance(
+      context: ThemedReactContext
+  ): ReactHorizontalScrollContainerLegacyView = ReactHorizontalScrollContainerLegacyView(context)
 
   public companion object {
     public const val REACT_CLASS: String = "AndroidHorizontalScrollContentView"
-    private @UIManagerType var uiManagerType: Int? = null
   }
 }
