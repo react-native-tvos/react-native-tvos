91958a32f83d84a0ecb56215b2d5712cebef04cf
Douglas Lowder <douglowder@mac.com>
2025-07-18 13:56:18 -0700

    TV: script change for RNTester tvOS build

--- packages/rn-tester/modify-hermes-engine-for-rn-tester.sh
+++ packages/rn-tester/modify-hermes-engine-for-rn-tester.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sed -i '' 's/\"react-native\"/\"react-native-tvos\"/;' ../react-native/sdks/hermes-engine/hermes-engine.podspec
+sed -i '' 's/\"react-native\"/\"react-native-tvos\"/;' ../react-native/third-party-podspecs/ReactNativeDependencies.podspec
