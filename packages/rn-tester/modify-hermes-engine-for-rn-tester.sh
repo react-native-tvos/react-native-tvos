bbbc5c9aac38fb213b6211a6a29176406574d50c
Douglas Lowder <douglowder@mac.com>
2025-07-23 00:02:03 -0700

    TV: Workflows and build profiles for release builds

--- packages/rn-tester/modify-hermes-engine-for-rn-tester.sh
+++ packages/rn-tester/modify-hermes-engine-for-rn-tester.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# No longer used, but may be needed again in the future
+# This script is needed when building RNTester for tvOS in release branches only.
 
-#sed -i '' 's/\"react-native\"/\"react-native-tvos\"/;' ../react-native/sdks/hermes-engine/hermes-engine.podspec
-#sed -i '' 's/\"react-native\"/\"react-native-tvos\"/;' ../react-native/third-party-podspecs/ReactNativeDependencies.podspec
+if [[ "$RNTV_RELEASE_BUILD" == "1" ]]; then
+  sed -i '' 's/\"react-native\"/\"react-native-tvos\"/;' ../react-native/sdks/hermes-engine/hermes-engine.podspec
+  sed -i '' 's/\"react-native\"/\"react-native-tvos\"/;' ../react-native/third-party-podspecs/ReactNativeDependencies.podspec
+fi
