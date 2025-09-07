c51e3d93aa53c6a32afae34130a946209ae0ed72
Douglas Lowder <douglowder@mac.com>
2025-09-07 21:06:38 +0000

    Bump version number (0.81.1-3)

--- packages/react-native/React/Base/RCTVersion.m
+++ packages/react-native/React/Base/RCTVersion.m
@@ -24,7 +24,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(81),
                   RCTVersionPatch: @(1),
-                  RCTVersionPrerelease: [NSNull null],
+                  RCTVersionPrerelease: @"3",
                   };
   });
   return __rnVersion;
--- packages/react-native/ReactCommon/cxxreact/ReactNativeVersion.h
+++ packages/react-native/ReactCommon/cxxreact/ReactNativeVersion.h
@@ -22,7 +22,7 @@ constexpr struct {
   int32_t Major = 0;
   int32_t Minor = 81;
   int32_t Patch = 1;
-  std::string_view Prerelease = "";
+  std::string_view Prerelease = "3";
 } ReactNativeVersion;
 
 } // namespace facebook::react
