9b8800a5e5193a34c23a7e47ff05a6bb5e83cc42
Douglas Lowder <douglowder@mac.com>
2025-09-17 12:36:59 -0700

    Move Apple codesign utility, add expired cert check

--- tools/rntv-workflows/post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is used to install the custom functions for the EAS build profiles
-# It is run as part of the post-install step of yarn
-
-# Fail if anything errors
-set -eox pipefail
-
-(cd .eas/build/importCodesignCerts && yarn && yarn build)
