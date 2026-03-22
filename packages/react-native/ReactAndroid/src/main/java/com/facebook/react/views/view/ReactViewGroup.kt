622e6fdac0e1fcc09a69607a17feaa0dad4ae6e6
Ruud Burger <ruud@plexapp.com>
2026-03-22 04:12:56 +0100

    fix: remove non-null assertion on getFirstFocusableView return value (#1076)
    The !! operator caused a NullPointerException crash when no focusable element was found in a view group. All callers already handle null.

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewGroup.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewGroup.kt
@@ -634,7 +634,7 @@ public open class ReactViewGroup public constructor(context: Context?) :
       index++
     }
 
-    return firstFocusableElement!!
+    return firstFocusableElement
   }
 
   private fun moveFocusToFirstFocusable(viewGroup: ReactViewGroup): Boolean {
