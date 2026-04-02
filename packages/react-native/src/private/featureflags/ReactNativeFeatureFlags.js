fc93752db143d9aa7dc95edad53df25c7fa55160
Yusuf YILDIRIM <yusuf@tinythin.gs>
2026-04-02 23:28:20 +0200

    Disable enableCustomFocusSearchOnClippedElementsAndroid feature flag (#1090)

--- packages/react-native/src/private/featureflags/ReactNativeFeatureFlags.js
+++ packages/react-native/src/private/featureflags/ReactNativeFeatureFlags.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<474b28e49cdb8a46a361a32df3c92eed>>
+ * @generated SignedSource<<c7dd7afe6bc76c1abb2b0499d3740ec2>>
  * @flow strict
  * @noformat
  */
@@ -279,7 +279,7 @@ export const enableCppPropsIteratorSetter: Getter<boolean> = createNativeFlagGet
 /**
  * This enables the fabric implementation of focus search so that we can focus clipped elements
  */
-export const enableCustomFocusSearchOnClippedElementsAndroid: Getter<boolean> = createNativeFlagGetter('enableCustomFocusSearchOnClippedElementsAndroid', true);
+export const enableCustomFocusSearchOnClippedElementsAndroid: Getter<boolean> = createNativeFlagGetter('enableCustomFocusSearchOnClippedElementsAndroid', false);
 /**
  * Enables destructor calls for ShadowTreeRevision in the background to reduce UI thread work.
  */
