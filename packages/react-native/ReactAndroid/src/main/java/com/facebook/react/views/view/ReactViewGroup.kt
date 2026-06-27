8c7d0b3ef0dbfeb3e1f0ab1539cbcb7070fd32a8
Aubrey Taylor <github@aubrey.email>
2026-06-27 21:38:34 +1000

    fix(android): re-attempt focus if target isn't initially attached (#1137)

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewGroup.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewGroup.kt
@@ -458,7 +458,7 @@ public open class ReactViewGroup public constructor(context: Context?) :
 
   internal fun requestFocusFromJS() {
     if (isAttachedToWindow) {
-      super.requestFocus(FOCUS_DOWN, null)
+      requestFocus(FOCUS_DOWN, null)
     } else {
       focusOnAttach = true
     }
