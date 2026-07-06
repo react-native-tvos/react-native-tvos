b86f13e56c52f54be04bcce3a2157a2496951849
Mateusz Mędrek <luigimario1913@gmail.com>
2026-07-06 21:28:37 +0200

    fix talkback focus guide handling after Kotlin conversion (#1132)
    After `ReactViewGroup` conversion to Kotlin, the logic to intercept a11y delegate events became wrongly executed only when there is non-null `Bundle` argument - as a result, the `destinations` or last focused element stopped receiving the events
    
    The previous logic implemented in java didn't care about the nullability of `Bundle` argument, as it was always just forwarded to `performAccessibilityAction` on the target views, so to fix and maintain correct behavior, the `handleInterceptEvents` signature now expects optional `Bundle` args argument

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewGroup.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewGroup.kt
@@ -1188,7 +1188,7 @@ public open class ReactViewGroup public constructor(context: Context?) :
           return super.performAccessibilityAction(host, action, args)
         }
         if (action == AccessibilityNodeInfo.ACTION_FOCUS) {
-          if (args?.let { host.interceptAccessibilityEvents(action, it) } == true) {
+          if (host.interceptAccessibilityEvents(action, args) == true) {
             return true
           }
           // Handle case when focus guide cannot find any focusable child
@@ -1206,7 +1206,7 @@ public open class ReactViewGroup public constructor(context: Context?) :
           return super.performAccessibilityAction(host, action, args)
         }
         if (action == AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS) {
-          if (args?.let { host.interceptAccessibilityEvents(action, it) } == true) {
+          if (host.interceptAccessibilityEvents(action, args) == true) {
             return true
           }
           // Handle case when focus guide cannot find any focusable child
@@ -1265,7 +1265,7 @@ public open class ReactViewGroup public constructor(context: Context?) :
     return this.isFocusGuideTalkbackAccessibilityDelegateSet
   }
 
-  private fun interceptAccessibilityEvents(action: Int, args: Bundle): Boolean {
+  private fun interceptAccessibilityEvents(action: Int, args: Bundle?): Boolean {
     if (!this.isTVFocusGuide) {
       return false
     }
