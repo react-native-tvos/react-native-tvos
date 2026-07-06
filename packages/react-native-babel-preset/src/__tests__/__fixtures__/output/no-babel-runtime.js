a2cd37f828ad9338eeacd69a64d90c5c512035be
Davyd NRB <4661784+retyui@users.noreply.github.com>
2026-07-06 09:27:48 -0700

    Change default transform profile to 'hermes-stable' (#57208)
    Summary:
    Issue: https://github.com/react/react-native/issues/57174
    
    ## Changelog:
    
    [GENERAL] [CHANGED] - Change default transform profile to 'hermes-stable'
    
    Pull Request resolved: https://github.com/react/react-native/pull/57208
    
    Test Plan:
    ```
    js1 jest react-native-babel-preset
    Determining test suites to run...
     PASS   rn-oss  xplat/js/react-native-github/packages/react-native-babel-preset/src/__tests__/plugin-warn-on-deep-imports-test.js
     PASS   rn-oss  xplat/js/react-native-github/packages/react-native-babel-preset/src/__tests__/transform-snapshot-test.js
    
    Test Suites: 2 passed, 2 total
    Tests:       55 passed, 55 total
    Snapshots:   6 passed, 6 total
    Time:        7.13 s
    Ran all test suites matching /react-native-babel-preset/i.
    ```
    
    Reviewed By: javache
    
    Differential Revision: D110766109
    
    Pulled By: robhogan
    
    fbshipit-source-id: 2964283239c44128e310400e1cd2b0321f0403be

--- packages/react-native-babel-preset/src/__tests__/__fixtures__/output/no-babel-runtime.js
+++ packages/react-native-babel-preset/src/__tests__/__fixtures__/output/no-babel-runtime.js
@@ -44,26 +44,16 @@ var _jsxRuntime = require("react/jsx-runtime");
 var _dataUtils = require("./data-utils");
 function _interopRequireWildcard(e, t) { if ("function" == typeof WeakMap) var r = new WeakMap(), n = new WeakMap(); return (_interopRequireWildcard = function (e, t) { if (!t && e && e.__esModule) return e; var o, i, f = { __proto__: null, default: e }; if (null === e || "object" != typeof e && "function" != typeof e) return f; if (o = t ? n : r) { if (o.has(e)) return o.get(e); o.set(e, f); } for (var _t in e) "default" !== _t && {}.hasOwnProperty.call(e, _t) && ((i = (o = Object.defineProperty) && Object.getOwnPropertyDescriptor(e, _t)) && (i.get || i.set) ? o(f, _t, i) : f[_t] = e[_t]); return f; })(e, t); }
 function _wrapRegExp() { _wrapRegExp = function (e, r) { return new BabelRegExp(e, void 0, r); }; var e = RegExp.prototype, r = new WeakMap(); function BabelRegExp(e, t, p) { var o = RegExp(e, t); return r.set(o, p || r.get(e)), _setPrototypeOf(o, BabelRegExp.prototype); } function buildGroups(e, t) { var p = r.get(t); return Object.keys(p).reduce(function (r, t) { var o = p[t]; if ("number" == typeof o) r[t] = e[o];else { for (var i = 0; void 0 === e[o[i]] && i + 1 < o.length;) i++; r[t] = e[o[i]]; } return r; }, Object.create(null)); } return _inherits(BabelRegExp, RegExp), BabelRegExp.prototype.exec = function (r) { var t = e.exec.call(this, r); if (t) { t.groups = buildGroups(t, this); var p = t.indices; p && (p.groups = buildGroups(p, this)); } return t; }, BabelRegExp.prototype[Symbol.replace] = function (t, p) { if ("string" == typeof p) { var o = r.get(this); return e[Symbol.replace].call(this, t, p.replace(/\$<([^>]+)(>|$)/g, function (e, r, t) { if ("" === t) return e; var p = o[r]; return Array.isArray(p) ? "$" + p.join("$") : "number" == typeof p ? "$" + p : ""; })); } if ("function" == typeof p) { var i = this; return e[Symbol.replace].call(this, t, function () { var e = arguments; return "object" != typeof e[e.length - 1] && (e = [].slice.call(e)).push(buildGroups(e, i)), p.apply(this, e); }); } return e[Symbol.replace].call(this, t, p); }, _wrapRegExp.apply(this, arguments); }
+function _inherits(t, e) { if ("function" != typeof e && null !== e) throw new TypeError("Super expression must either be null or a function"); t.prototype = Object.create(e && e.prototype, { constructor: { value: t, writable: !0, configurable: !0 } }), Object.defineProperty(t, "prototype", { writable: !1 }), e && _setPrototypeOf(t, e); }
+function _setPrototypeOf(t, e) { return _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function (t, e) { return t.__proto__ = e, t; }, _setPrototypeOf(t, e); }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
 function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
 function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
-function _callSuper(t, o, e) { return o = _getPrototypeOf(o), _possibleConstructorReturn(t, _isNativeReflectConstruct() ? Reflect.construct(o, e || [], _getPrototypeOf(t).constructor) : o.apply(t, e)); }
-function _possibleConstructorReturn(t, e) { if (e && ("object" == typeof e || "function" == typeof e)) return e; if (void 0 !== e) throw new TypeError("Derived constructors may only return object or undefined"); return _assertThisInitialized(t); }
-function _assertThisInitialized(e) { if (void 0 === e) throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); return e; }
-function _isNativeReflectConstruct() { try { var t = !Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); } catch (t) {} return (_isNativeReflectConstruct = function () { return !!t; })(); }
-function _getPrototypeOf(t) { return _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function (t) { return t.__proto__ || Object.getPrototypeOf(t); }, _getPrototypeOf(t); }
-function _inherits(t, e) { if ("function" != typeof e && null !== e) throw new TypeError("Super expression must either be null or a function"); t.prototype = Object.create(e && e.prototype, { constructor: { value: t, writable: !0, configurable: !0 } }), Object.defineProperty(t, "prototype", { writable: !1 }), e && _setPrototypeOf(t, e); }
-function _setPrototypeOf(t, e) { return _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function (t, e) { return t.__proto__ = e, t; }, _setPrototypeOf(t, e); }
 function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
 function _asyncToGenerator(n) { return function () { var t = this, e = arguments; return new Promise(function (r, o) { var a = n.apply(t, e); function _next(n) { asyncGeneratorStep(a, r, o, _next, _throw, "next", n); } function _throw(n) { asyncGeneratorStep(a, r, o, _next, _throw, "throw", n); } _next(void 0); }); }; }
-function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
-function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
-function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 function _classPrivateFieldLooseBase(e, t) { if (!{}.hasOwnProperty.call(e, t)) throw new TypeError("attempted to use private field on non-instance"); return e; }
 var id = 0;
 function _classPrivateFieldLooseKey(e) { return "__private_" + id++ + "_" + e; }
@@ -75,9 +65,8 @@ function _OverloadYield(e, d) { this.v = e, this.k = d; }
 var _count = _classPrivateFieldLooseKey("count");
 var _instances = _classPrivateFieldLooseKey("instances");
 var _increment = _classPrivateFieldLooseKey("increment");
-var Counter = exports.Counter = function () {
-  function Counter() {
-    _classCallCheck(this, Counter);
+class Counter {
+  constructor() {
     Object.defineProperty(this, _increment, {
       value: _increment2
     });
@@ -87,23 +76,17 @@ var Counter = exports.Counter = function () {
     });
     _classPrivateFieldLooseBase(Counter, _instances)[_instances]++;
   }
-  return _createClass(Counter, [{
-    key: "value",
-    get: function () {
-      return _classPrivateFieldLooseBase(this, _count)[_count];
-    }
-  }, {
-    key: "increment",
-    value: function increment() {
-      _classPrivateFieldLooseBase(this, _increment)[_increment]();
-    }
-  }], [{
-    key: "instanceCount",
-    get: function () {
-      return _classPrivateFieldLooseBase(Counter, _instances)[_instances];
-    }
-  }]);
-}();
+  get value() {
+    return _classPrivateFieldLooseBase(this, _count)[_count];
+  }
+  increment() {
+    _classPrivateFieldLooseBase(this, _increment)[_increment]();
+  }
+  static get instanceCount() {
+    return _classPrivateFieldLooseBase(Counter, _instances)[_instances];
+  }
+}
+exports.Counter = Counter;
 function _increment2() {
   _classPrivateFieldLooseBase(this, _count)[_count]++;
 }
@@ -139,57 +122,36 @@ function _fetchData() {
 function getNestedValue(obj) {
   return obj?.a?.b?.c ?? 42;
 }
-var _age = _classPrivateFieldLooseKey("age");
-var Animal = exports.Animal = function () {
-  function Animal(name, age) {
-    _classCallCheck(this, Animal);
-    Object.defineProperty(this, _age, {
-      writable: true,
-      value: void 0
-    });
+class Animal {
+  #age;
+  constructor(name, age) {
     this.name = name;
-    _classPrivateFieldLooseBase(this, _age)[_age] = age;
+    this.#age = age;
   }
-  return _createClass(Animal, [{
-    key: "speak",
-    value: function speak() {
-      return `${this.name} makes a sound`;
-    }
-  }, {
-    key: "age",
-    get: function () {
-      return _classPrivateFieldLooseBase(this, _age)[_age];
-    }
-  }]);
-}();
-var Dog = exports.Dog = function (_Animal2) {
-  function Dog(name, age, breed) {
-    var _this;
-    _classCallCheck(this, Dog);
-    _this = _callSuper(this, Dog, [name, age]);
-    _this.breed = breed;
-    return _this;
+  speak() {
+    return `${this.name} makes a sound`;
   }
-  _inherits(Dog, _Animal2);
-  return _createClass(Dog, [{
-    key: "speak",
-    value: function speak() {
-      return `${this.name} barks!`;
-    }
-  }, {
-    key: "fetchTreats",
-    value: function () {
-      var _fetchTreats = _asyncToGenerator(function* () {
-        yield new Promise(resolve => setTimeout(resolve, 100));
-        return ['bone', 'biscuit', 'toy'];
-      });
-      function fetchTreats() {
-        return _fetchTreats.apply(this, arguments);
-      }
-      return fetchTreats;
-    }()
-  }]);
-}(Animal);
+  get age() {
+    return this.#age;
+  }
+}
+exports.Animal = Animal;
+class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age);
+    this.breed = breed;
+  }
+  speak() {
+    return `${this.name} barks!`;
+  }
+  fetchTreats() {
+    return _asyncToGenerator(function* () {
+      yield new Promise(resolve => setTimeout(resolve, 100));
+      return ['bone', 'biscuit', 'toy'];
+    })();
+  }
+}
+exports.Dog = Dog;
 function processUser({
   name,
   age = 18,
@@ -245,10 +207,12 @@ function matchEmoji(text) {
   var match = text.match(/(?:[#\*0-9\xA9\xAE\u203C\u2049\u2122\u2139\u2194-\u2199\u21A9\u21AA\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA\u24C2\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE\u2600-\u2604\u260E\u2611\u2614\u2615\u2618\u261D\u2620\u2622\u2623\u2626\u262A\u262E\u262F\u2638-\u263A\u2640\u2642\u2648-\u2653\u265F\u2660\u2663\u2665\u2666\u2668\u267B\u267E\u267F\u2692-\u2697\u2699\u269B\u269C\u26A0\u26A1\u26A7\u26AA\u26AB\u26B0\u26B1\u26BD\u26BE\u26C4\u26C5\u26C8\u26CE\u26CF\u26D1\u26D3\u26D4\u26E9\u26EA\u26F0-\u26F5\u26F7-\u26FA\u26FD\u2702\u2705\u2708-\u270D\u270F\u2712\u2714\u2716\u271D\u2721\u2728\u2733\u2734\u2744\u2747\u274C\u274E\u2753-\u2755\u2757\u2763\u2764\u2795-\u2797\u27A1\u27B0\u27BF\u2934\u2935\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55\u3030\u303D\u3297\u3299]|\uD83C[\uDC04\uDCCF\uDD70\uDD71\uDD7E\uDD7F\uDD8E\uDD91-\uDD9A\uDDE6-\uDDFF\uDE01\uDE02\uDE1A\uDE2F\uDE32-\uDE3A\uDE50\uDE51\uDF00-\uDF21\uDF24-\uDF93\uDF96\uDF97\uDF99-\uDF9B\uDF9E-\uDFF0\uDFF3-\uDFF5\uDFF7-\uDFFF]|\uD83D[\uDC00-\uDCFD\uDCFF-\uDD3D\uDD49-\uDD4E\uDD50-\uDD67\uDD6F\uDD70\uDD73-\uDD7A\uDD87\uDD8A-\uDD8D\uDD90\uDD95\uDD96\uDDA4\uDDA5\uDDA8\uDDB1\uDDB2\uDDBC\uDDC2-\uDDC4\uDDD1-\uDDD3\uDDDC-\uDDDE\uDDE1\uDDE3\uDDE8\uDDEF\uDDF3\uDDFA-\uDE4F\uDE80-\uDEC5\uDECB-\uDED2\uDED5-\uDED7\uDEDC-\uDEE5\uDEE9\uDEEB\uDEEC\uDEF0\uDEF3-\uDEFC\uDFE0-\uDFEB\uDFF0]|\uD83E[\uDD0C-\uDD3A\uDD3C-\uDD45\uDD47-\uDDFF\uDE70-\uDE7C\uDE80-\uDE89\uDE8F-\uDEC6\uDECE-\uDEDC\uDEDF-\uDEE9\uDEF0-\uDEF8])/);
   return match?.[0];
 }
-var MyClass = exports.MyClass = _createClass(function MyClass(value) {
-  _classCallCheck(this, MyClass);
-  this.value = value;
-});
+var MyClass = class {
+  constructor(value) {
+    this.value = value;
+  }
+};
+exports.MyClass = MyClass;
 function loadModule() {
   return _loadModule.apply(this, arguments);
 }
