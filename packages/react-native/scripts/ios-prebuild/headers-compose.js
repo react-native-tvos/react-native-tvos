207717ab9cf0b10fc8b7e5f69b284994ce8bbecf
Douglas Lowder <douglowder@mac.com>
2026-07-26 19:56:17 -0700

    Include tvOS slices in composed RNCore headers

--- packages/react-native/scripts/ios-prebuild/headers-compose.js
+++ packages/react-native/scripts/ios-prebuild/headers-compose.js
@@ -37,6 +37,7 @@ const {
 const {
   CATALYST_STUB_SLICE,
   DEFAULT_STUB_SLICES,
+  TV_STUB_SLICES,
   buildDepsHeadersXcframework,
   composeHeadersOnlyXcframework,
 } = require('./headers-xcframework');
@@ -297,8 +298,8 @@ function buildReactNativeHeadersXcframework(
 
   // ---- compose (stub archives + create-xcframework) ----
   const slices = includeCatalyst
-    ? [...DEFAULT_STUB_SLICES, CATALYST_STUB_SLICE]
-    : DEFAULT_STUB_SLICES;
+    ? [...DEFAULT_STUB_SLICES, ...TV_STUB_SLICES, CATALYST_STUB_SLICE]
+    : [...DEFAULT_STUB_SLICES, ...TV_STUB_SLICES];
   const outXcfw = composeHeadersOnlyXcframework(
     outDir,
     'ReactNativeHeaders',
