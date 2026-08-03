2bb56dc94bc13820d31692baa051c96c0371027f
Ruslan Lesiutin <hoxy@meta.com>
2026-08-03 05:40:14 -0700

    Add `enableConditionalUseWarning` React feature flag (#57783)
    Summary:
    Pull Request resolved: https://github.com/react/react-native/pull/57783
    
    # Changelog: [Internal]
    
    Adds the `enableConditionalUseWarning` dynamic React feature flag, which the React renderer reads from `ReactNativeInternalFeatureFlags`. The flag gates the new warning for conditional use of `use()` based on cache state: https://react.dev/warnings/conditional-use-of-use
    
    This is a dev-only warning, I don't see any reasons to control this via gk.
    
    Reviewed By: antonk52
    
    Differential Revision: D114407789
    
    fbshipit-source-id: d63dd09180e6560a82d178610c7ae24b9e31b65e

--- private/react-native-fantom/runtime/mocks/ReactNativeInternalFeatureFlags.js
+++ private/react-native-fantom/runtime/mocks/ReactNativeInternalFeatureFlags.js
@@ -13,4 +13,5 @@ module.exports = {
   enableFragmentRefsInstanceHandles: true,
   enableFragmentRefsTextNodes: true,
   enableViewTransitionForPersistenceMode: true,
+  enableConditionalUseWarning: true,
 };
